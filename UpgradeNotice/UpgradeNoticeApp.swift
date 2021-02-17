b9cee43aabdbdfc989a522a6b49ea68400001221
TMS <munbai6666@gmail.com>
2021-02-17 10:21:16 +0900

    Initial Commit

---
+++ UpgradeNotice/UpgradeNoticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  UpgradeNoticeApp.swift
+//  UpgradeNotice
+//
+//  Created by 田中義男 on 2021/02/17.
+//
+
+import SwiftUI
+
+@main
+struct UpgradeNoticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
